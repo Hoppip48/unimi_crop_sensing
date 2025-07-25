e643520a84422e128f2840279eaea68eb9cc4a5f
Francesco <francescomorgi2015@gmail.com>
2025-07-25 19:29:35 +0200

    general optimization

--- crop_sensing/main.py
+++ crop_sensing/main.py
@@ -1,18 +1,13 @@
-import cv2
 import zed_manager
 import find_plant
 import cobot_manager
+import create_plc
 
-def save_clustered_image(image, bounding_boxes):
-    # Draw bounding boxes on the original image for visualization
-    for (x_min, y_min, x_max, y_max) in bounding_boxes:
-        cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
-    cv2.imwrite("crop_sensing/data/clusters.png", image)
-
+# This function is used to test the functionalities of the crop sensing module
 def main():
-    # init magic numbers
+    # testing parameters
     linux_ip = "192.168.5.6"
-    plants_number = 3
+    plants_number = 8
     
     # Ottieni translazione e orientamento del cobot
     pose = cobot_manager.get_cobot_pose(linux_ip)
@@ -21,25 +16,26 @@ def main():
     zed = zed_manager.zed_init(pose)
     
     # Acquisisci i dati dalla ZED
-    image, depth_map, normal_map, point_cloud = zed_manager.get_zed_image(zed)
-    #image, depth_map, normal_map, point_cloud = mock.get_image()
-    
+    image, depth_map, normal_map, point_cloud = zed_manager.get_zed_image(zed, save=True)
+
     # Trova le piantine nell'immagine
-    mask = find_plant.filter_plants(image)
+    mask = find_plant.filter_plants(image, save_mask=True)
     
     # Dividi la maschera di ogni piantina
     masks, bounding_boxes = find_plant.segment_plants(mask, plants_number)
-    save_clustered_image(image, bounding_boxes)
+    find_plant.save_clustered_image(image, bounding_boxes)
 
     # Estrai le bounding box 3D dei cluster
     for m in masks:
         bbxpts = find_plant.plot_3d_bbox(m, point_cloud)
         
-    # Comunicala al Cobot e acquisci ply
+    # Comunicala al Cobot [nel caso sia in una macchina differente]
     #cobot_manager.send_cobot_map(linux_ip, bbxpts)
-    
-    # Analizza ply
-    
+
+    # Crea plc [da chiamare quando si intende memorizzare la plc di un video]
+    #create_plc.record_and_save(plant_name='piantina1', frames=300)
+
+    #zed.close()
 
 
 if __name__ == "__main__":
